8b6f588c06a7be89a8b94113c7aa049b209ec91c
matt <philmattdev@gmail.com>
2021-11-24 09:48:29 +0800

    implemented hashing to store nsfw classification results based on hash

--- apis/nsfw_detection.py
+++ apis/nsfw_detection.py
@@ -6,11 +6,14 @@ import os
 import tempfile
 from typing import TYPE_CHECKING
 
+import imagehash
 import moviepy.editor as mp
 from flask import Blueprint, request
 from fluxhelper.flask import makeResponse
 from library.schemas import *
 from nudenet import NudeClassifier
+from PIL import Image
+from videohash.videohash import VideoHash
 
 if TYPE_CHECKING:
     from ..app import Server
@@ -65,6 +68,17 @@ def constructNsfw(server: "Server") -> Blueprint:
         suffix = None
         if data["contentType"].lower() == "image/gif":
             suffix = ".gif"
+        elif data["contentType"].lower().startswith("video/"):
+            mappings = {
+                "video/x-msvideo": ".avi",
+                "video/mp4": ".mp4",
+                "video/mpeg": ".mpeg",
+                "video/ogg": ".ogv",
+                "video/mp2t": ".ts",
+                "video/webm": ".webm",
+                "video/x-matroska": ".mkv"
+            }
+            suffix = mappings.get(data["contentType"].lower(), ".mp4")
 
         with tempfile.NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
             path = tmp.name
@@ -88,32 +102,55 @@ def constructNsfw(server: "Server") -> Blueprint:
                 data["contentType"] = "video/mp4"
 
             results["contentType"] = data["contentType"]
+            
+            # Get hash and attempt to get its results from the database if it exists
+            fromHash = False
+            if method == "img":
+                contentHash = str(imagehash.average_hash(Image.open(path)))
+            elif method == "vid":
+                contentHash = VideoHash(path).hash_hex
+            
+            results["hash"] = contentHash
 
-            # Perform classification
-            try:
-                nudenet = NudeClassifier()
+            stored = server.db.classified_nsfw.find_one({"hash": contentHash})
+            if stored:
+                server.logging.debug(f"Using hash: {contentHash}")
 
-                if method == "img":
-                    method = nudenet.classify
-                elif method == "vid":
-                    method = nudenet.classify_video
-                    
-                res = method(path, batch_size=2)
-                del nudenet
-                gc.collect()
+                fromHash = True
+                stored.pop("_id", None)
+                
+                response = makeResponse(data=stored)
 
-                if data["contentType"].startswith("video/"):
-                    newPreds = {}
-                    for k, v in res["preds"].items():
-                        newPreds[k] = {
-                            "safe": float(v["safe"]),
-                            "unsafe": float(v["unsafe"])
-                        }
-                    res["preds"] = newPreds
-                results["data"] = res
-                response = makeResponse(data=results)
-            except Exception as e:
-                response = makeResponse(status=500, msg=str(e))
+            if not fromHash:
+                # Perform classification
+                try:
+                    nudenet = NudeClassifier()
+
+                    if method == "img":
+                        method = nudenet.classify
+                    elif method == "vid":
+                        method = nudenet.classify_video
+
+                    res = method(path, batch_size=2)
+                    del nudenet
+                    gc.collect()
+
+                    if data["contentType"].startswith("video/"):
+                        newPreds = {}
+                        for k, v in res["preds"].items():
+                            newPreds[str(k)] = {
+                                "safe": float(v["safe"]),
+                                "unsafe": float(v["unsafe"])
+                            }
+                        res["preds"] = newPreds
+                    results["data"] = res
+
+                    server.db.classified_nsfw.insert_one(results)
+                    results.pop("_id", None)
+
+                    response = makeResponse(data=results)
+                except Exception as e:
+                    response = makeResponse(status=500, msg=str(e))
 
             # Delete
             if oldPath:
