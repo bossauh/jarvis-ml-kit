4d8254fa51c7fec4c3c3ecb4478a2f8f42366d1e
matt <philmattdev@gmail.com>
2021-11-30 07:48:26 +0800

    disable monitoring on cloud

--- app.py
+++ app.py
@@ -136,7 +136,7 @@ class Server(Monitor):
 
 def getApp() -> Flask:
     """Initialize the server with cloud settings and return the app. This is used by gunicorn."""
-    server = Server(None, doMonitor=True)
+    server = Server(None, doMonitor=False)
     return server.run()
 
 
