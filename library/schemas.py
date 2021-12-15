983297ef641a6b1fd94dfe02e6b5dd96603ffea1
matt <philmattdev@gmail.com>
2021-12-16 03:01:50 +0800

    minimal schema update

--- library/schemas.py
+++ library/schemas.py
@@ -1,7 +1,6 @@
 from schema import Schema, And, Use, Optional
 
 NSFW_DETECTION_CLASSIFY = Schema({
-    "bytes": And(Use(str)),
-    "contentType": And(Use(str)),
+    "url": And(Use(str)),
     Optional("ratelimitKey"): And(Use(str))
 })
